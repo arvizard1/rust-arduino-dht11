fcce73949e5713280b39c3c69f84e6ec3a774999
Aravind Kumar <arvizard@hotmail.com>
2021-01-29 20:58:59 +0800

    more chagnes

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,13 @@ use arduino_uno::hal::port::mode::TriState;
 
 use panic_halt as _;
 
-const TIMEOUT: u32 = 200;
 struct Dht<'a> {
     pin: &'a mut PD5<TriState>
 }
 
 
 impl<'a>  Dht<'a> {
-    fn get_readings(&mut self) -> Result<(u8,u8), (u8)> {
+    fn get_readings(&mut self) -> Result<(u8,u8), u8> {
         let mut lows = [0;41];
         let mut highs = [0;41];
         let mut humidity:u8 = u8::MAX;
@@ -27,17 +26,21 @@ impl<'a>  Dht<'a> {
         }
         
         for n in 1..41 {
-            lows[n] = self.expect_pulse(false);
-            highs[n] = self.expect_pulse(true);
+            lows[n] = match self.expect_pulse(false){
+                Ok(duration) => duration,
+                _ => break,
+            };
+            highs[n] =match self.expect_pulse(true) {
+                Ok(duration) => duration,
+                _ => break,
+            };
         }
         let mut o: u8 =0;
         for n in 1..41 {
-            if highs[n] < lows[n] {
-                o = o << 1;
+            o = o << 1;
+            if highs[n] > lows[n] {
+                o |=1;
             }
-            else {
-                o= (o << 1) | 1;
-            }    
             if n%8 ==0 {               
                 if n/8 == 1 {
                      humidity = o;
@@ -53,36 +56,33 @@ impl<'a>  Dht<'a> {
                }
                if n/8 == 5 {
                 if (humidity + humidity_decimal + temperature + temperature_decimal) == o  {
-                    return Ok((humidity, temperature));
+                    return Ok((humidity , temperature));
                 }
-                }
-                
+                }  
                 o=0;
             }
-
     }
     return Err(1);
     }
-    fn expect_pulse(&mut self, level: bool) -> u32 {
-        const TIMEOUT: u32 = 200;
-        let mut counter: u32 = 0;
+    fn expect_pulse(&mut self, level: bool) -> Result<u8, u8> {
+        let mut counter: u8 = 0;
         if level {
             while self.pin.is_high().void_unwrap() {
-                counter = counter +1;
-                if counter > TIMEOUT {
-                    return TIMEOUT;
+                counter +=1;
+                if counter > u8::MAX {
+                    return Err(1);
                 }
             }
         }
         else {
             while self.pin.is_low().void_unwrap() {
-                counter = counter +1;
-                if counter > TIMEOUT {
-                    return TIMEOUT;
+                counter +=1;
+                if counter > u8::MAX {
+                    return Err(1);
                 }
             }
         }
-        counter
+        Ok(counter)
     }
 
     fn initialize_dht(&mut self) -> bool {
@@ -91,13 +91,17 @@ impl<'a>  Dht<'a> {
         self.pin.set_high().void_unwrap();
         arduino_uno::delay_us(40);
     
-        let low =self.expect_pulse(false);
-        let high = self.expect_pulse(true);
-        if low !=TIMEOUT && high !=TIMEOUT {
-
-            return true;
-        }
-        return false;
+        match self.expect_pulse(false) {
+            Ok(_) => {
+                match self.expect_pulse(true) {
+                    Ok(_) => return true,
+                    _ => return false
+                };
+            },
+            _ => return false,
+            
+        };
+        
     }
 }
 
@@ -112,18 +116,19 @@ fn main() -> ! {
         pins.d1.into_output(&mut pins.ddr),
         9600.into_baudrate(),
     );
-    ufmt::uwriteln!(&mut serial, "Hello from Rusty Arduino!\r").void_unwrap();
     let mut dht = pins.d5.into_tri_state(&mut pins.ddr);
-
-
+    ufmt::uwriteln!(&mut serial, "HRllo\r").void_unwrap();
     loop {
-        nb::block!(serial.read()).void_unwrap();
+        let b= nb::block!(serial.read()).void_unwrap();
+        ufmt::uwriteln!(&mut serial, "{}\r",b).void_unwrap();
         let mut temp_humidity = Dht {
             pin: &mut dht
         };
         match temp_humidity.get_readings() {
             Ok((h, t)) => ufmt::uwriteln!(&mut serial, "{} {}!\r", h,t).void_unwrap(),
-            _ => continue,
+            _ => {
+                ufmt::uwriteln!(&mut serial, "Failed!\r").void_unwrap();
+                continue},
         }
      
 
